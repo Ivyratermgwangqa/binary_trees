dab75d8ca9eacbbbbe38dd290842f8d1ef478584
root <Ivyratermgwangqa>
2024-01-04 19:43:13 -0800

    :update

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,6 +1,6 @@
 
 /**
- * binary_tree_levelorder - Traverses a binary tree using level-order traversal.
- * @tree: Pointer to the root node of the tree to traverse.
- * @func: Pointer to a function to call for each node.
- */
+* binary_tree_levelorder - Traverses a binary tree using level-order traversal.
+* @tree: Pointer to the root node of the tree to traverse.
+* @func: Pointer to a function to call for each node.
+*/
