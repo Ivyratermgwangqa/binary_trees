7ea9e87a21df5e3c41d1805c74e67cff3e2aaae3
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-06 02:35:03 +0200

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,3 +12,19 @@ if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 return (NULL);
 return (binary_tree_sibling(node->parent));
 }
+/**
+ * binary_tree_sibling - Finds the sibling of a node.
+ * @node: A pointer to the node to find the sibling.
+ *
+ * Return: A pointer to the sibling node, or NULL if no sibling is found.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL)
+return (NULL);
+
+if (node->parent->left == node)
+return (node->parent->right);
+else
+return (node->parent->left);
+}
